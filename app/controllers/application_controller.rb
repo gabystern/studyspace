1a72d1ca23f005c01a11e87962c689abe1d5add3
gabystern <stern.gabrielle@gmail.com>
2017-05-08 18:42:44 -0400

    added validation & current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :authorize_user, :current_user, :logged_in?
+
+
+private
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def authrize_user
+    if !logged_in?
+      flash[:notice] = "You are not allowed to do that unless you are logged in."
+      redirect_to login_path
+    end
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+
+  root '/' => 'study_rooms#index'
+
   resources :user_study_rooms
   resources :categories
   resources :locations
   resources :study_rooms
   resources :users
 
-  get 'sessions/new' => 'sessions#new'
+  get 'sessions/new' => 'sessions#new', as: 'login'
   post 'sessions/new' => 'sessions#create'
 
   # get 'users/new' => 'users#new'
