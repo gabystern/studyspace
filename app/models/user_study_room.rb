c7a9cd52af431ca20e7b23f80c82b6c6247b0cac
github username <github email address>
2017-05-08 23:23:57 -0400

    user profiles with study rooms hosting/attending and seed

--- app/models/user_study_room.rb
+++ app/models/user_study_room.rb
@@ -1,10 +1,4 @@
 class UserStudyRoom < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :study_room
-  belongs_to :user
-=======
   belongs_to :user
   belongs_to :study_room
-  
->>>>>>> associations working
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Location.create(name: 'Flatiron', address: '11 Broadway')
+Location.create(name: 'Starbucks', address: '25 Broadway')
+Location.create(name: 'Central Park', address: '52 Central Park West')
+Location.create(name: "Joe's Place", address: '34 Broadway')
+
+Category.create(name: 'Math')
+Category.create(name: 'History')
+Category.create(name: 'Ruby')
+Category.create(name: "Knitting")
+
+StudyRoom.create(category_id: 1, location_id: 1)
+StudyRoom.create(category_id: 1, location_id: 2)
+StudyRoom.create(category_id: 2, location_id: 1)
+StudyRoom.create(category_id: 3, location_id: 3)
+StudyRoom.create(category_id: 4, location_id: 2)
+
+
+User.create(username: "MariKats", password: "123", password_confirmation: "123")
+User.create(username: "Boo", password: "123", password_confirmation: "123")
+User.create(username: "Lola", password: "123", password_confirmation: "123")
+User.create(username: "Jimmy", password: "123", password_confirmation: "123")
+User.create(username: "EasyE", password: "123", password_confirmation: "123")
+
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 1)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 2)
+UserStudyRoom.find_or_create_by(user_id: 2, study_room_id: 3)
+UserStudyRoom.find_or_create_by(user_id: 4, study_room_id: 2)
+UserStudyRoom.find_or_create_by(user_id: 4, study_room_id: 3)
+UserStudyRoom.find_or_create_by(user_id: 3, study_room_id: 4)
+UserStudyRoom.find_or_create_by(user_id: 5, study_room_id: 2)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 1)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 3, owner: true)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 4, owner: true)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 3, owner: true)
+UserStudyRoom.find_or_create_by(user_id: 1, study_room_id: 5, owner: true)
