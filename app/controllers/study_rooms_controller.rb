5415024585d867dd50db69d1bad01304ae0c5b02
kylewstewart <kyle.w.stewart@mac.com>
2017-05-09 09:19:56 -0400

    in progress

--- app/controllers/study_rooms_controller.rb
+++ app/controllers/study_rooms_controller.rb
@@ -1,19 +1,29 @@
 class StudyRoomsController < ApplicationController
 
+
   def index
     @study_rooms = StudyRoom.all
   end
 
   def new
-
+    @study_room = StudyRoom.new
+    @locations = Location.all
+    @categories = Category.all
   end
 
   def create
+    byebug
 
   end
 
   def show
+    @study_room = StudyRoom.find(params[:id])
+  end
 
+  private
+
+  def study_room_params(*args)
+    params.require(:study_room).permit(*args)
   end
 
 end
