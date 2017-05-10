755653a605eef19612c871411b248dd72fd55d53
kylewstewart <kyle.w.stewart@mac.com>
2017-05-10 09:02:56 -0400

    2nd

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,8 +5,24 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
 
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.create(location_params)
+    redirect_to location_path(@location)
+  end
+
   def show
     @location = Location.find(params[:id])
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :address, :wifi, :capacity, :volume)
+
   end
 
 
